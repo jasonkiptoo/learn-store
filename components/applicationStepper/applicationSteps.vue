ad95f090454540adbddc2bd50eda1cecf7bfe7ad
jasonkiptoo <kiptoojason@gmail.com>
2024-04-07 05:06:21 +0300

    update to configs

--- components/applicationStepper/applicationSteps.vue
+++ components/applicationStepper/applicationSteps.vue
@@ -1,38 +1,86 @@
 <template>
-   <div class="card flex row">
-        <div class="flex mb-2 gap-2 justify-center">
-            <Button @click="active = 0" rounded label="1" class="w-2rem h-2rem p-0" :outlined="active !== 0" />
-            <Button @click="active = 1" rounded label="2" class="w-2rem h-2rem p-0" :outlined="active !== 1" />
-            <Button @click="active = 2" rounded label="3" class="w-2rem h-2rem p-0" :outlined="active !== 2" />
-        </div>
-        <Steps v-model:activeStep="active" :model="items" />
-    </div>
+    <Card>
+        <template #content>
+
+
+            <Stepper linear>
+                <StepperPanel header="Personal Info">
+                    <template #content="{ nextCallback }">
+                        <div class="main flex  mb-4">
+                            <PersonalInfo/>
+
+                        </div>
+                        <div class="flex pt-4 justify-content-end">
+                            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback" />
+                        </div>
+                    </template>
+                </StepperPanel>
+                <StepperPanel header="Header II">
+                    <template #content="{ prevCallback, nextCallback }">
+                        <div class="flex flex-column h-12rem">
+                            <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">Content II</div>
+                        </div>
+                        <div class="flex pt-4 justify-content-between">
+                            <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="prevCallback" />
+                            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback" />
+                        </div>
+                    </template>
+                </StepperPanel>
+                <StepperPanel header="Header III">
+                    <template #content="{ prevCallback }">
+                        <div class="flex flex-column h-12rem">
+                            <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">Content III</div>
+                        </div>
+                        <div class="flex pt-4 justify-content-start">
+                            <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="prevCallback" />
+                        </div>
+                    </template>
+                </StepperPanel>
+            </Stepper>
+        </template>
+    </Card>
 </template>
-
-<script setup>
-
-import { ref } from "vue";
-
-const active = ref(0);
-
-const items = ref([
-    {
-        label: 'Personal Info'
-    },
-    {
-        label: 'Reservation'
-    },
-    {
-        label: 'Review'
-    }
-]);
-
-</script>
-
-<style scoped>
-
-.card{
-    background-color: red;
-}
-
-</style>
+  
+  <script setup>
+  import { ref } from "vue";
+  import PersonalInfo from '@/components/personalInfo/personalInfo.vue'
+  
+  const active = ref(0);
+  
+  const personalInfo = ref({
+    name: ''
+  });
+  
+  const reservationInfo = ref({
+    date: ''
+  });
+  
+  const reviewInfo = ref({
+    comment: ''
+  });
+  
+  const items = ref([
+      {
+          label: 'Personal Info'
+      },
+      {
+          label: 'Reservation'
+      },
+      {
+          label: 'Review'
+      }
+  ]);
+  </script>
+  
+  <style scoped>
+  .card{
+      background-color: red;
+  }
+  .step-content {
+    margin-top: 20px;
+  }
+  .main{
+    margin-bottom: 20px;
+  }
+  </style>
+  
---
+++ components/personalInfo/personalInfo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      
+        <label for="name">Name:</label>
+          <input type="text" id="name" v-model="personalInfo.name">
+
+
+          <div class="flex pt-4 justify-content-end">
+                            <!-- <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback" /> -->
+                        </div>
+          <!-- Add one field for each tab -->
+
+    </div>
+</template>
+
+<script setup>
+
+const personalInfo =ref({
+    name: ""
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
